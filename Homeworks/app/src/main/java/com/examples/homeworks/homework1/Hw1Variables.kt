ca857aa57df635276ad4e1d1e96a7012c4dd70e5
HalitBatuhanASLAN <halitbatuhanaslan@gmail.com>
2025-04-27 15:02:33 +0300

    Updated hw1

--- Homeworks/app/src/main/java/com/examples/homeworks/homework1/Hw1Variables.kt
+++ Homeworks/app/src/main/java/com/examples/homeworks/homework1/Hw1Variables.kt
@@ -1,50 +1,142 @@
 package com.examples.homeworks.homework1
 
-fun main() {
+fun main()
+{
     val city = "Ankara"
+    println("City: $city")
+
     val country = "Turkey"
+    println("Country: $country")
+
     val phoneNumber = "+90 555 555 555 555"
+    println("Phone Number: $phoneNumber")
+
     val postalCode = 123456789
+    println("Postal Code: $postalCode")
+
     val email = "aaaaaaa"
+    println("Email: $email")
+
     val job = "computer engineering"
+    println("Job: $job")
+
     val stock = 1907
+    println("Stock: $stock")
+
     val customerName = "Our customer"
+    println("Customer Name: $customerName")
+
     val balance = 1234.567
+    println("Balance: $balance")
+
     val birthday = "19.07.1907"
+    println("Birthday: $birthday")
+
     val salary = 30000
+    println("Salary: $salary")
+
     val meritalStatus = "single"
+    println("Merital Status: $meritalStatus")
+
     val commentOfProduct = "Product a great value for money"
+    println("Comment Of Product: $commentOfProduct")
+
     val paymentDate = "23.04.2025"
+    println("Payment Date: $paymentDate")
+
     val payment = true
+    println("Payment: $payment")
+
     val ordered = 500
+    println("Ordered: $ordered")
+
     val carModel = "Ford"
+    println("Car Model: $carModel")
+
     val bookName = "Nutuk"
+    println("Book Name: $bookName")
+
     val publishingDate = "10.10.2010"
+    println("Publishing Date: $publishingDate")
+
     val rateOfDiscount = "%25"
+    println("Rate Of Discount: $rateOfDiscount")
+
     val numberOfRooms = 4
+    println("Number Of Rooms: $numberOfRooms")
+
     val latitude = "15degree"
+    println("Latitude: $latitude")
+
     val longitude = "23degree"
+    println("Longitude: $longitude")
+
     val productName = "Book"
+    println("Product Name: $productName")
+
     val priceOfFood = 450
+    println("Price Of Food: $priceOfFood")
+
     val brand = "Mavi"
+    println("Brand: $brand")
+
     val nameOfMusic = "izmirin daglarinda cicekler acar"
+    println("Name Of Music: $nameOfMusic")
+
     val durationOfVideo = "19.29"
+    println("Duration Of Video: $durationOfVideo")
+
     val pointOfProduct = "4.7"
+    println("Point Of Product: $pointOfProduct")
+
     val pictureName = "my_photo.png"
+    println("Picture Name: $pictureName")
+
     val fileFormat = ".kt"
+    println("File Format: $fileFormat")
+
     val color = "Yellow"
+    println("Color: $color")
+
     val colorCode = "#000080"
+    println("Color Code: $colorCode")
+
     val phoneModel = "Iphone"
+    println("Phone Model: $phoneModel")
+
     val screenSize = 13.4
+    println("Screen Size: $screenSize")
+
     val height = 92.6
+    println("Height: $height")
+
     val nationalDay = "may 19 the commemoration of atatürk youth and sports day"
+    println("National Day: $nationalDay")
+
     val holiday = "Sunday"
+    println("Holiday: $holiday")
+
     val bookingDate = "16.06 Monday"
+    println("Booking Date: $bookingDate")
+
     val streetName = "Ankara Street"
+    println("Street Name: $streetName")
+
     val busRoute= "490E"
+    println("Bus Route: $busRoute")
+
     val remainingMinutes = 13
+    println("Remaining Minutes: $remainingMinutes")
+
     val trackingCode = 1934234234
+    println("Tracking Code: $trackingCode")
+
     val couponDuration = "8 days"
+    println("Coupon Duration: $couponDuration")
+
     val couponCode = "a43B323"
+    println("Coupon Code: $couponCode")
+
     val billingAdress = "Ankara,Turkey"
+    println("Billing Adress: $billingAdress")
 }
